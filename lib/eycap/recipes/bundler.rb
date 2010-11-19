9310a19963aa312aa7a6bbf97621694998a2c64b
Matthew Bennink <matt@soomopublishing.com>
2010-11-19 12:48:51 -0500

    Modified bundler task to install bundle in shared directory and symlink to release directory.

--- lib/eycap/recipes/bundler.rb
+++ lib/eycap/recipes/bundler.rb
@@ -2,7 +2,9 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :bundler do
     desc "Automatically installed your bundled gems if a Gemfile exists"
     task :bundle_gems do
-      run "if [ -f #{release_path}/Gemfile ]; then cd #{release_path} && bundle install --without=test,development; fi"
+      # run "if [ -f #{release_path}/Gemfile ]; then cd #{release_path} && bundle install --without=test,development; fi"
+      run "if [ -f #{release_path}/Gemfile ]; then cd #{release_path} && bundle install --path #{shared_path}/bundled_gems --deployment --without development test; fi"
+      run "if [ -f #{release_path}/Gemfile ]; then ln -nfs #{shared_path}/bundled_gems #{latest_release}/vendor/bundle; fi"
     end
     after "deploy:symlink_configs","bundler:bundle_gems"
   end
